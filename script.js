b63810f4c30c0595396038d1c36feaede999aedd
Ebere Okezie <ebereokezie@gmail.com>
2022-03-24 08:03:27 -0500

    add action on tabs

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by Javascript";
-document.querySelector("body").appendChild(h2);
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks; 
+    tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
